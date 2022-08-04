09452194d0e726178499e14bd131065d06aeebdc
LIG_JY <dldlsry053@naver.com>
2022-08-04 18:52:37 +0900

    PS

---
+++ HackerRank/Print_the_Eoa_Linkedlist.py
@@ -0,0 +1,26 @@
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def printLinkedList(head):
+    current = llist.head
+    while current:
+        print(current.data)
+        current = current.next
