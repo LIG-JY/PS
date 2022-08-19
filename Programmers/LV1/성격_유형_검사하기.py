16c966364d23f7fd5282861333f5c4b064821285
LIG_JY <dldlsry053@naver.com>
2022-08-19 13:15:05 +0900

    PS

---
+++ Programmers/LV1/성격_유형_검사하기.py
@@ -0,0 +1,31 @@
+문제링크: https://school.programmers.co.kr/learn/courses/30/lessons/118666?language=python3
+
+def scoreboard(surveys, choices):
+    score = dict()
+    categories = ['R', 'T', 'C', 'F', 'J', 'M', 'A', 'N']
+    for i in range(len(categories)):
+        score[categories[i]] = 0
+
+    for i in range(len(choices)):
+        if choices[i] > 4:
+            score[surveys[i][1]] += choices[i] - 4
+        elif choices[i] < 4:
+            score[surveys[i][0]] += 4 - choices[i]
+    return score
+
+def solution(survey, choices):
+    answer = ''
+    sb = scoreboard(survey, choices)
+    mbti = {1: ['R', 'T'], 2: ['C', 'F'], 3: ['J', 'M'], 4: ['A', 'N']}
+    for key in mbti:
+        first = mbti[key][0]
+        second = mbti[key][1]
+        if sb[first] > sb[second]:
+            answer = answer + first
+        elif sb[first] < sb[second]:
+            answer = answer + second
+        else:
+            temp = [first,second]
+            temp.sort()
+            answer = answer + temp[0]
+    return answer
