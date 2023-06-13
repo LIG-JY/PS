a1586d7452ef59189581ba9d2bb60c5ea04a232c
gyo <dldlsry053@naver.com>
2023-06-13 21:08:08 +0900

    230613

---
+++ boj/cpp/11655-a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string problem, uppercases, lowercases;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	for (int i = 0; i < 26; i++) {
+		uppercases += (i + 'A');
+		lowercases += (i + 'a');
+	}
+	getline(cin, problem);
+	for (auto e : problem) {
+		if (('a' <= e && e <= 'z')) {
+			cout << lowercases[(lowercases.find(e) + 13) % 26];
+		}
+		else if (('A' <= e && e <= 'Z')) {
+			cout << uppercases[(uppercases.find(e) + 13) % 26];
+		}
+		else {
+			cout << e;
+		}
+	}
+	return 0;
+}
