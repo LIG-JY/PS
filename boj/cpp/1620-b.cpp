1249a7c6ace580143b3c2454126bd1be3a2d1052
gyo <dldlsry053@naver.com>
2023-06-15 01:36:39 +0900

    230614

---
+++ boj/cpp/1620-b.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int N, M;
+string name, key;
+map<string,int> d1;
+map<int,string> d2;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        cin >> name;
+        d1[name] = i;
+        d2[i] = name;
+    }
+    for (int i = 1; i <= M; i++) {
+        cin >> key;
+        if (atoi(key.c_str()) != 0) {
+            cout << d2[atoi(key.c_str())] << "\n";
+        }
+        else {
+            cout << d1[key] << "\n";
+        }
+    }
+    return 0;
+}
