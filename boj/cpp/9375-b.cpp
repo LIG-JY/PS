38d37d62283bf69003d2e77e8f6b4cc0fe4ddc99
gyo <dldlsry053@naver.com>
2023-06-15 02:49:03 +0900

    230614

---
+++ boj/cpp/9375-b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+string c, c_type;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    while (n--) {
+        cin >> m;
+        map<string,int> d;
+        for (int i = 0; i < m; i++) {
+            cin >> c >> c_type;
+            d[c_type]++;
+        }
+        long long res = 1;
+        for (auto e : d) {
+            res *= (long long)(e.second + 1);
+        }
+        cout << res - 1 << "\n";
+    }
+    return 0;
+}
