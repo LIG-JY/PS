ee91b4f5f3359dbd699b6481eb4526da198fd821
gyo <dldlsry053@naver.com>
2023-06-14 16:07:49 +0900

    230614

---
+++ boj/kotlin/Boj2309-a.kt
@@ -0,0 +1,47 @@
+import java.io.*
+
+class Boj2309 {
+
+}
+
+val fake = ArrayList<Int>()
+fun findFake(dwarfs: ArrayList<Int>, sum: Int) {
+    for (i in 0 until 9) {
+        for (j in i + 1 until 9) {
+            if (sum - dwarfs[i] - dwarfs[j] == 100) {
+                fake.add(dwarfs[j])
+                fake.add(dwarfs[i])
+                return
+            }
+        }
+    }
+}
+
+fun a() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    var sum = 0
+    val dwarfs = ArrayList<Int>()
+
+    repeat(9) {
+        val num = br.readLine().toInt();
+        sum += num
+        dwarfs.add(num)
+    }
+    dwarfs.sort()
+    findFake(dwarfs, sum)
+    for (e in dwarfs) {
+        if (e in fake) {
+            continue
+        }
+        bw.write(e.toString())
+        bw.newLine()
+    }
+    bw.flush()
+    bw.close()
+    br.close()
+}
+
+fun main() {
+    a()
+}
