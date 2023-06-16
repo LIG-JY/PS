8229a629f0e200d282c2c6f9bab46e9aecbe9514
gyo <dldlsry053@naver.com>
2023-06-16 23:08:42 +0900

    230616

---
+++ boj/cpp/1940-b.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int cnt, N, M, materials[15001];
+vector<int> ret;
+void comb(int m[], int depth) {
+    if (ret.size() == 2) {
+        if (ret[0] + ret[1] == M) {
+            cnt++;
+        }
+        return;
+    }
+    for (int i = depth + 1; i < N; i++) {
+        ret.push_back(m[i]);
+        comb(m, i);
+        ret.pop_back();
+    }
+    return;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> materials[i];
+    }
+    comb(materials, -1);
+    cout << cnt << "\n";
+    return 0;
+}
