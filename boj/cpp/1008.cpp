f0cbd6806851238386bcd15521f8f389b8d97ebd
gyo <dldlsry053@naver.com>
2023-06-17 03:29:41 +0900

    230616

---
+++ boj/cpp/1008.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a,b;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> a >> b;
+    cout.precision(10);
+    cout << (double) a / b << "\n";
+}
