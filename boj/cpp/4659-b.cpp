e5d6b7a5509642c0bd22dfb12ce2a4ca54d6c982
gyo <dldlsry053@naver.com>
2023-07-05 16:04:42 +0900

    230705

---
+++ boj/cpp/4659-b.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<char, int> voweltable;
+string s;
+int c_cnt, v_cnt;
+
+bool checkFun(string s) {
+    bool containVowel = false;
+    bool triple = false;
+    bool dup = false;
+
+    c_cnt = v_cnt = 0;
+    char prevC = -1;
+    for (char c : s) {
+        if (!containVowel && voweltable[c]) {
+            containVowel = true;
+        }
+        if (voweltable[c]) {
+            if (!v_cnt) {
+                c_cnt = 0;
+            }
+            v_cnt++;
+        }
+        if (!voweltable[c]) {
+            if (!c_cnt) {
+                v_cnt = 0;
+            }
+            c_cnt++;
+        }
+        if (c_cnt == 3 || v_cnt == 3) {
+            triple = true;
+            break;
+        }
+        if ((c == prevC) && (c != 'e' && c != 'o')) {
+            dup = true;
+            break;
+        }
+        prevC = c;
+    }
+    return containVowel && !triple && !dup;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for (char i = 'a'; i <= 'z'; i++) {
+        if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u') {
+            voweltable[i] = 1;
+            continue;
+        }
+        voweltable[i] = 0;
+    } //table setting
+
+    while (cin >> s) {
+        if (s == "end") {
+            break;
+        }
+        if (checkFun(s)) {
+            cout << "<" << s << "> is acceptable." << "\n";
+            continue;
+        }
+        cout << "<" << s << "> is not acceptable." << "\n";
+    }
+    return 0;
+}
