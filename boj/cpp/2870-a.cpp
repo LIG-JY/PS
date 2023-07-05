8d9960bf961301b2ae4a320e8e4fe2957940d63b
gyo <dldlsry053@naver.com>
2023-07-05 18:03:56 +0900

    230705

---
+++ boj/cpp/2870-a.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+string input;
+vector<string> res;
+
+bool cmp(const string& a, const string& b) {
+    if (a.size() == b.size()) {
+        return a < b;
+    }
+    return a.size() < b.size();
+}
+
+void putToken(string token) {
+    char c = token[0];
+    while (token.size() > 1 && c == '0') {
+        token = token.substr(1);
+        c = token[0];
+    }
+    res.push_back(token);
+    return;
+}
+
+void play(string s) {
+    string token = "";
+    int range = s.size();
+    for (int i = 0; i < range; i++) {
+        char c = s[i];
+        if (c >= '0' && c <= '9') {
+            token += c;
+            if (i == range - 1) {
+                putToken(token);
+            }
+            continue;
+        }
+        if (!token.empty()) {
+            putToken(token);
+        }
+        token = "";
+    }
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> input;
+        play(input);
+    }
+    sort(res.begin(), res.end(), cmp);
+    for (auto num : res) {
+        cout << num << "\n";
+    }
+    return 0;
+}
