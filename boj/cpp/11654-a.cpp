09b4c70c1c8525184604f9cf42f9f6b223ff65f0
gyo <dldlsry053@naver.com>
2023-08-20 22:37:52 +0900

    230820

---
+++ boj/cpp/11654-a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char a;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> a;
+	cout << int(a) << "\n"; // char -> ASCII
+}
