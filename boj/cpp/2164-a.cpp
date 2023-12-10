07224ef87b30075c2fcb74717d539365c384aed0
LIG-JY <dldlsry053@naver.com>
2023-12-10 21:00:35 +0900

    231210

---
+++ boj/cpp/2164-a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+deque<int> _list;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        _list.push_back(i);
+    }
+
+    int last;
+    while (!_list.empty())
+    {
+        // 젤 위 카드 버리기
+        last = _list.front();
+        _list.pop_front();
+
+        // 젤 위 카드 아래로 옮기기
+        _list.push_back(_list.front());
+        _list.pop_front();
+    }
+    cout << last;
+}
