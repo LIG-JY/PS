f0b24bb4722d18bc61d706168f300c213d99f661
LIG-JY <dldlsry053@naver.com>
2024-12-14 03:23:00 +0900

    solution

--- boj/kotlin/S16920/16920.kt
+++ boj/kotlin/S16920/16920.kt
@@ -6,76 +6,69 @@ import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
 class S16920 {
-    data class Point(val y: Int, val x: Int)
 
+    companion object {
+        const val UNVISITED = '.'
+        const val WALL = '#'
+    }
+
+    data class Point(val y: Int, val x: Int)
     data class Node(val p: Point, val step: Int)
 
-    val directions = listOf(Point(1, 0), Point(-1, 0), Point(0, 1), Point(0, -1))
+    val directions = listOf(Point(0, 1), Point(0, -1), Point(1, 0), Point(-1, 0))
 
     fun main() {
         BufferedReader(InputStreamReader(System.`in`)).use { reader ->
             BufferedWriter(OutputStreamWriter(System.out)).use { writer ->
                 val (n, m, p) = reader.readLine().split(" ").map { it.toInt() }
-                val maxSteps = mutableListOf<Int>() // 1-based index
-                maxSteps.add(Int.MIN_VALUE)
+                val maxSteps = ArrayDeque<Int>()
                 maxSteps.addAll(reader.readLine().split(" ").map { it.toInt() })
+                maxSteps.addFirst(Int.MIN_VALUE)
                 val grid = Array(n) { reader.readLine().toCharArray() }
-                val visited = Array(n) { BooleanArray(m) { false } }
+                val playerCastleCounts = IntArray(p + 1) { 0 }
                 val playerQueues = Array(p + 1) { ArrayDeque<Node>() }
 
-                for (y in grid.indices) {
-                    for (x in grid[y].indices) {
-                        if (grid[y][x].isDigit()) {
-                            visited[y][x] = true
-                            playerQueues[grid[y][x].digitToInt()].addLast(Node(Point(y, x), 0))
+                for (y in 0 until n) {
+                    for (x in 0 until m) {
+                        val v = grid[y][x]
+                        if (v.isDigit()) {
+                            playerCastleCounts[v.digitToInt()]++
+                            playerQueues[v.digitToInt()].addLast(Node(Point(y, x), 0))
                         }
                     }
                 }
 
-                while (true) {
-                    for (i in 1..p) {
-                        val playerQueue = playerQueues[i]
+                while (!playerQueues.all { it.isEmpty() }) {
+                    for (playerIndex in 1..p) {
+                        val playerQueue = playerQueues[playerIndex]
                         val nextQueue = ArrayDeque<Node>()
-
+                        // bfs
                         while (playerQueue.isNotEmpty()) {
                             val current = playerQueue.removeFirst()
-                            val point = current.p
-                            val step = current.step
+                            val currentPoint = current.p
+                            val currentStep = current.step
 
-                            if (step >= maxSteps[i]) {
-                                nextQueue.addLast(Node(point, 0))
+                            if (current.step >= maxSteps[playerIndex]) {
+                                nextQueue.addLast(Node(currentPoint, 0))
                                 continue
                             }
 
                             for (d in directions) {
-                                val ny = point.y + d.y
-                                val nx = point.x + d.x
-
-                                if (ny in 0 until n && nx in 0 until m && !visited[ny][nx] && grid[ny][nx] != '#') {
-                                    visited[ny][nx] = true
-                                    grid[ny][nx] = i.digitToChar()
-                                    playerQueue.addLast(Node(Point(ny, nx), step + 1))
+                                val ny = currentPoint.y + d.y
+                                val nx = currentPoint.x + d.x
+                                if (ny in 0 until n && nx in 0 until m && grid[ny][nx] != WALL && grid[ny][nx] == UNVISITED) {
+                                    playerCastleCounts[playerIndex]++
+                                    grid[ny][nx] = playerIndex.digitToChar()
+                                    playerQueue.addLast(Node(Point(ny, nx), currentStep + 1))
                                 }
                             }
                         }
-                        playerQueues[i] = nextQueue
-                    }
-
-                    if (playerQueues.all { it.isEmpty() }) {
-                        break
+                        playerQueues[playerIndex] = nextQueue
                     }
                 }
 
-                for (num in 1..p) {
-                    var count = 0
-                    for (y in grid.indices) {
-                        for (x in grid[y].indices) {
-                            val v = grid[y][x]
-                            if (v.isDigit() && v.digitToInt() == num) {
-                                ++count
-                            }
-                        }
-                    }
+                for (playerIndex in 1..p) {
+                    val count = playerCastleCounts[playerIndex]
                     writer.write("$count ")
                 }
             }
