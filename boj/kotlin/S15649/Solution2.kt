5a17703c1233135dcc6403528c4c9985d5ca4310
LIG-JY <dldlsry053@naver.com>
2025-01-09 11:46:42 +0900

    solve

---
+++ boj/kotlin/S15649/Solution2.kt
@@ -0,0 +1,39 @@
+package kotlin.S15649
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+class Solution2 {
+    fun main() {
+        BufferedReader(InputStreamReader(System.`in`)).use { reader ->
+            BufferedWriter(OutputStreamWriter(System.out)).use { writer ->
+                val (n, m) = reader.readLine().split(" ").map { it.toInt() }
+                val output = IntArray(m)
+
+                fun solveRecursive(used: BooleanArray, size: Int) {
+                    if (size == m) {
+                        writer.writeLine(output.joinToString(" "))
+                        return
+                    }
+                    for (i in 1..n) {
+                        if (used[i]) continue
+                        used[i] = true
+                        output[size] = i
+                        solveRecursive(used, size + 1)
+                        used[i] = false
+                    }
+                }
+
+                val used = BooleanArray(n + 1) { false }
+                solveRecursive(used, 0)
+            }
+        }
+    }
+
+    private fun BufferedWriter.writeLine(v: Any) {
+        this.write("$v")
+        this.newLine()
+    }
+}
