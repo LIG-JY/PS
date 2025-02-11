6e1cc6093d10f0b947d3d1b742ba6fcea5d61941
LIG-JY <dldlsry053@naver.com>
2025-02-11 13:00:57 +0900

    solve 13335-1

---
+++ boj/kotlin/S13335/Solution1.kt
@@ -0,0 +1,47 @@
+package kotlin.S13335
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+class Solution1 {
+    fun main() {
+        BufferedReader(InputStreamReader(System.`in`)).use { reader ->
+            BufferedWriter(OutputStreamWriter(System.out)).use { writer ->
+                val (n, w, l) = reader.readLine().split(" ").map { it.toInt() }
+                val weights = reader.readLine().trim().split(" ").map { it.toInt() }
+                assert(weights.size == n)
+
+                val truckEntryTime = IntArray(n)
+                val inBridge = ArrayDeque<Int>(n)
+                inBridge.addLast(0)
+                truckEntryTime[0] = 1
+
+                var weightSum = weights[inBridge.first()]
+                var latestOut = -1
+                for (i in 1 until n) {
+                    val nextWeight = weights[i]
+                    while (weightSum + nextWeight > l || inBridge.size > w) {
+                        latestOut = inBridge.removeFirst()
+                        weightSum -= weights[latestOut]
+                    }
+                    inBridge.addLast(i)
+                    weightSum += weights[i]
+                    if (inBridge.first() == 0) {
+                        truckEntryTime[i] = truckEntryTime[i - 1] + 1
+                    } else {
+                        truckEntryTime[i] = maxOf(truckEntryTime[i - 1] + 1, truckEntryTime[latestOut] + w)
+                    }
+                }
+
+                writer.writeLine(truckEntryTime[n - 1] + w)
+            }
+        }
+    }
+
+    private fun BufferedWriter.writeLine(v: Any) {
+        this.write("$v")
+        this.newLine()
+    }
+}
